4f16c52c1e083ea63a57fec3a5b623584ad24582
Rifqi Firlian <firlianrifqi22@gmail.com>
2023-03-21 18:32:31 +0700

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -6,23 +6,12 @@ import KRSRI from './components/KRSRI/KRSRI';
 import KRTMI from './components/KRTMI/KRTMI';
 import {HashRouter, Route, Routes} from "react-router-dom"
 import KRTI from './components/KRTI/KRTI';
+import Home from './components/Home';
 
 
 function App() {
   return (
-    <div>
-      <HashRouter>
-        <Routes>
-          <Route exact path='/' Component={HomePage}/>
-          <Route exact path='KRAI' Component={KRAI}/>
-          <Route exact path='KRSBI' Component={KRSBI}/>
-          <Route exact path='KRSTI' Component={KRSTI}/>
-          <Route exact path='KRSRI' Component={KRSRI}/>
-          <Route exact path='KRTMI' Component={KRTMI}/>
-          <Route exact path='KRTI' Component={KRTI}/>
-        </Routes>
-      </HashRouter>
-    </div>
+    <HomePage/>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<App />
+    <HashRouter>
+    <App />
+    </HashRouter>
 );
