dd0ea862f3fe2979772e033e78c86b691f0fa21f
Rifqi Firlian <firlianrifqi22@gmail.com>
2023-03-21 18:43:18 +0700

    stable version on localhost

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import KRSBI from './components/KRSBI/KRSBI';
 import KRSTI from './components/KRSTI/KRSTI';
 import KRSRI from './components/KRSRI/KRSRI';
 import KRTMI from './components/KRTMI/KRTMI';
-import {HashRouter, Route, Routes} from "react-router-dom"
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 import KRTI from './components/KRTI/KRTI';
-import Home from './components/Home';
 
 
 function App() {
   return (
-    <HomePage/>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<HomePage/>}/>
+          <Route exact path='/KRAI' element={<KRAI/>}/>
+          <Route exact path='/KRSBI' element={<KRSBI/>}/>
+          <Route exact path='/KRSTI' element={<KRSTI/>}/>
+          <Route exact path='/KRSRI' element={<KRSRI/>}/>
+          <Route exact path='/KRTMI' element={<KRTMI/>}/>
+          <Route exact path='/KRTI' element={<KRTI/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
